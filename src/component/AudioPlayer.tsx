a0760ab4b1dc103124ea9b2c21bf8d6e2ba38928
Evgenii M <mez.e.s@yandex.ru>
2020-10-18 00:22:27 +0300

    Add icons to audio player buttons

--- src/component/AudioPlayer.tsx
+++ src/component/AudioPlayer.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './AudioPlayer.css'
-import {Button, Dropdown} from "semantic-ui-react";
-import { Icon } from 'semantic-ui-react'
+import {Dropdown} from "semantic-ui-react";
 
 const ActionsDropdownMenu = () => (
     <Dropdown text='Actions' className="actions-dropdown-menu">
@@ -15,13 +14,13 @@ const ActionsDropdownMenu = () => (
 function AudioPlayer() {
     return (
       <div className="audio-player-container" id="audioPlayerContainer">
-          <Button className="audio-player-button previous">Prev</Button>
-          <Button className="audio-player-button play">Play</Button>
-          <Button className="audio-player-button next">Next</Button>
+          <div className="previous audio-player-button"/>
+          <div className="play audio-player-button"/>
+          <div className="next audio-player-button"/>
           <input type="range" min="1" max="100" defaultValue="50" className="audio-player-slider"/>
           <ActionsDropdownMenu/>
-          <Button className="audio-player-button like">Like</Button>
-          <Icon className="audio-player-icon" name='volume up' size='large'/>
+          <div className="like audio-player-button"/>
+          <div className="audio-player-icon"/>
           <input type="range" min="1" max="100" defaultValue="90" className="volume-slider audio-player-slider"/>
       </div>
     )
